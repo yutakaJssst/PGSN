ab39c0f1fc99bfadbfe1e1a1c1d43a14972d2012
Yoriyuki Yamagata <yoriyuki.y@gmail.com>
2025-02-17 15:17:01 +0900

    CC CAP-A formalization (not work yet)

---
+++ examples/cap-a.py
@@ -0,0 +1,62 @@
+import sys
+
+import pgsn_term
+
+sys.path.append("..")
+import json
+from gsn_term import *
+import gsn
+
+y = variable('y')
+xs = variable('xs')
+c = variable('c')
+
+cs = list_term((string("C1"), string("C2"), string("C3")))
+
+test_docs = variable('test_docs')
+test_resources = variable('test_resources')
+evidences = variable('evidences')
+
+goals = variable('goals')
+immediate = lambda_abs(goals, strategy(description="immediate", sub_goals=goals))
+
+evd = variable('evidence')
+evidence_as_goal = lambda_abs(evd, goal(description=evd('description'), support=evd))
+
+xs = variable('xs')
+
+gd3 = lambda_abs(xs,
+                 let_vars(
+                     (
+                     (test_docs, map_term(lambda_abs(c,
+                                                     evidence_as_goal(
+                                                         evidence(description=format_string("Test doc for {c}",
+                                                                                            {'c':c})))), xs)),
+                    (test_docs, map_term(lambda_abs(c,
+                                                    evidence_as_goal(
+                                                        evidence(description=format_string("Test resources for {c}",
+                                                                                            {'c': c})))), xs)
+                     ),
+                     (evidences, cons(evidence_as_goal(evidence(description="E3.1...")),
+                                        concat(test_docs, test_resources)))
+                     ),
+                    goal(description="GD3: Security test is ...",
+                        context="N.D. devel. info. ...",
+                        support=immediate(evidences)
+                         )))
+
+cap_a_term = gd3(cs)
+
+
+# (λxs.goal(dsc:“GD3: Security test is ...”,
+# ctx:“N.D. devel. info. ...”,
+# supp: [ev(“E3.1:...”)] + +
+# map(λy.ev(“Test doc on Component {y}”), xs) + +
+# map(λy.ev(“Test resource on Component {y}”), xs)))
+# [C1, C2, . . . , Cn]
+
+if __name__ == '__main__':
+    cap_a_fully_eval = cap_a_term.fully_eval(steps=1000000)
+    print(cap_a_fully_eval)
+    n = gsn.pgsn_to_gsn(cap_a_fully_eval, steps=100000)
+    print(json.dumps(gsn.python_val(n), sort_keys=True, indent=4))
